815b5f403db69b9143528825cf9c894359caa977
Jifan Li <101528038+jifanli@users.noreply.github.com>
2023-09-07 11:18:46 -0500

    add R0v3

--- R/R0v3.R
+++ R/R0v3.R
@@ -1,4 +1,4 @@
-#' R0 for li23 and li20 model
+#' R0 for li23v3 model
 #'
 #' Calculate  R_0 = alpha beta D + (1-alpha) mu beta D
 #' for the model of Li et al
@@ -9,21 +9,21 @@
 #'
 #' @examples
 #' ## R0 for mle
-#' po_mle <- li23(U=5,version="MLEperiod3")
+#' po_mle <- li23v3(U=5,version="MLEperiod3")
 #' p_mle <- coef(po_mle)
-#' R0(p_mle,be=TRUE)
-#' R0(p_mle,be=FALSE)
+#' R0v3(p_mle,be=TRUE)
+#' R0v3(p_mle,be=FALSE)
 #' ## R0 for li20
-#' po_li <- li23(U=5,version="li20period3")
+#' po_li <- li23v3(U=5,version="li20period3")
 #' p_li <- coef(po_li)
-#' R0(p_li,be=TRUE)
-#' R0(p_li,be=FALSE)
+#' R0v3(p_li,be=TRUE)
+#' R0v3(p_li,be=FALSE)
 #'
 #' @export
-R0 <- function (params, be=T){
+R0v3 <- function (params, be=T){
  a = params[ifelse(be,"alpha_be1","alpha_af1")]
  b = params[ifelse(be,"Beta_be1","Beta_af1")]
- D = params[ifelse(be,"D_be1","D_af1")]
+ D = params["D1"]
  mu = params[ifelse(be,"mu_be1","mu_af1")]
  unname((a + (1-a) * mu) * b * D)
 }
